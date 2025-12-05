05cb3c6bc1fb0f71711790e1422329e93872c95a
Marvi <accounts.marvi@mailbox.org>
2025-12-05 15:26:51 +0100

    add solution for day05 part2

--- solutions/day05.py
+++ solutions/day05.py
@@ -1,3 +1,5 @@
+import time
+
 input: str = """3-5
 10-14
 16-20
@@ -26,7 +28,7 @@ def splitinput():
 
         if not delim:
             r = line.split("-")
-            ranges.append(((int(r[0]), int(r[1])+1)))
+            ranges.append(((int(r[0]), int(r[1]))))
         else:
             ids.append(int(line))
 
@@ -34,6 +36,7 @@ def splitinput():
 
 
 def part1():
+    start = time.time()
     sum: int = 0
     (ranges, ids) = splitinput()
 
@@ -42,8 +45,35 @@ def part1():
             if r[0] <= i <= r[1]:
                 sum += 1
                 break
+    end = time.time()
+    return (sum, end-start)
 
-    return sum
+
+def part2():
+    start = time.time()
+    sum: int = 0
+    (ranges, _) = splitinput()
+
+    ranges.sort()
+
+    left: int = 0
+    right: int = 0
+
+    for i in range(1, len(ranges)):
+        if ranges[left][0] < ranges[i][0] and ranges[right][1] > ranges[i][1]:
+            continue
+
+        if ranges[i][0] > ranges[right][1]:
+            sum += (ranges[right][1] - ranges[left][0]) + 1
+            left = i
+            right = i
+        else:
+            right = i
+
+    sum += (ranges[right][1] - ranges[left][0]) + 1
+    end = time.time()
+    return (sum, end - start)
 
 
 print("Part 1: ", part1())
+print("Part 2: ", part2())
