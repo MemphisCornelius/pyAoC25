eca8f4d9ced4585ff0e876dcd39d514b12bc150b
Marvi <accounts.marvi@mailbox.org>
2025-12-08 00:09:58 +0100

    make time correct

--- solutions/day07.py
+++ solutions/day07.py
@@ -19,15 +19,17 @@ input: str = """.......S.......
 
 input: str = open("inputs/day07", "r").read()
 
+input_a: list[str] = input.split("\n")
+for e in range(0, len(input_a)):
+    if not input_a[e]:
+        input_a.pop(e)
+
 
 def part1():
-    start: float = time.time()
+    start_t: float = time.time()
     sum: int = 0
 
-    diagram: list[str] = input.split("\n")
-    for e in range(0, len(diagram)):
-        if not diagram[e]:
-            diagram.pop(e)
+    diagram: list[str] = input_a.copy()
 
     start: int = diagram[0].index("S")
     diagram[1] = replace_at(diagram[1], start, "|")
@@ -43,8 +45,8 @@ def part1():
             else:
                 diagram[i + 1] = replace_at(diagram[i + 1], k, "|")
 
-    end: float = time.time()
-    return (sum, end - start)
+    end_t: float = time.time()
+    return (sum, end_t - start_t)
 
 
 def replace_at(s: str, i: int, r: str):
